8c252df7b8ec282ef75582d07d42bd060f938f65
MahdiaDaqiq <your_email_address@example.com>
2020-01-31 16:37:51 -0500

    Maddi's dumbo 1

---
+++ robot1.py
@@ -0,0 +1,29 @@
+#Evan Gray - January 2018
+import libpyAI as ai
+def AI_loop():
+  #Release keys
+  ai.thrust(0)
+  ai.turnLeft(0)
+  ai.turnRight(0)
+  #Set variables
+  heading = int(ai.selfHeadingDeg())
+  tracking = int(ai.selfTrackingDeg())
+  frontWall = ai.wallFeeler(500,heading)
+  leftWall = ai.wallFeeler(500,heading+90)
+  rightWall = ai.wallFeeler(500,heading-90)
+  trackWall = ai.wallFeeler(500,tracking)
+  #Thrust rules
+
+  if ai.selfSpeed() <= 1 and frontWall >= 20:
+    ai.thrust(1)
+  elif trackWall < 40:
+    ai.thrust(1)
+  #Turn rules
+  if leftWall < rightWall:
+    ai.turnRight(1)
+  else:
+    ai.turnLeft(1)
+
+  #Just keep shooting
+  ai.fireShot()
+ai.start(AI_loop,["-name","Dumbo","-join","cc8418"])
