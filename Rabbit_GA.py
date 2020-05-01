4ddf4e8dce9955f541204959ea2581bcd6b5ea7b
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-04-30 21:47:10 -0400

    Rabbit multiple chromosome and until it dies

--- Rabbit_GA.py
+++ Rabbit_GA.py
@@ -86,6 +86,7 @@ def crossover(new_population, chromosome_size, population_size, crossover_prob):
 			chromosome1 = chromosome1_part1 + chromosome1_part2
 			chromosome2 = chromosome2_part1 + chromosome2_part2
 			
+			print(parent1, parent2, chromosome1, chromosome2)
 			new_population.pop(parent1)
 			new_population.pop(parent2)
 
