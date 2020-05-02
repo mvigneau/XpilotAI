8253ecddaa46593cdd758b7983271cc0d68a58ba
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-01 20:57:14 -0400

    Implementation of Dumbster Fuzzy GA. Some Fuzzy but not completed 32 chrom

--- Dumpster_Learning.py
+++ Dumpster_Learning.py
@@ -64,10 +64,10 @@ def AI_loop():
   Distance_FarTopAlertValue = transform_fuzzy(Distance_CloseTopAlert, 50, Distance_CloseTopAlertValue, Distance_CloseTopAlertValue+(50*(2**len(Distance_CloseTopAlert)))) 
   print("Distance_FarTopAlertValue", Distance_FarTopAlertValue)
   Distance_CloseBottomAlert = current_chromosome[42:47]
-  Distance_CloseBottomAlertValue = transform_fuzzy(Distance_CloseBottomAlert, 50, Distance_CloseTopAlertValue, Distance_CloseTopAlertValue+(50*(2**len(Distance_CloseBottomAlert)))) 
+  Distance_CloseBottomAlertValue = transform_fuzzy(Distance_CloseBottomAlert, ((Distance_FarTopAlertValue - Distance_CloseTopAlertValue) // 32), Distance_CloseTopAlertValue, Distance_FarTopAlertValue) 
   print("Distance_CloseBottomAlertValue", Distance_CloseBottomAlertValue)
   Distance_FarBottomAlert = current_chromosome[47:52]
-  Distance_FarBottomAlertValue = transform_fuzzy(Distance_FarBottomAlert, 50, Distance_CloseTopAlertValue, Distance_FarTopAlertValue) 
+  Distance_FarBottomAlertValue = transform_fuzzy(Distance_FarBottomAlert, ((Distance_FarTopAlertValue - Distance_CloseTopAlertValue) // 32), Distance_CloseTopAlertValue, Distance_FarTopAlertValue) 
 
   #print("got pass the chrom")
 
