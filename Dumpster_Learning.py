71fef5ec488047744a19bbb1b918a78c76e9cf00
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-01 23:50:41 -0400

    Implementation of Dumbster Fuzzy GA. Some Fuzzy but not completed. Distance and Closing Rate Done

--- Dumpster_Learning.py
+++ Dumpster_Learning.py
@@ -35,11 +35,11 @@ def AI_loop():
   closingRate_SlowTopAlert = current_chromosome[0:4]
   closingRate_SlowTopAlertValue = transform_fuzzy(closingRate_SlowTopAlert, 1, 0, 16) 
   closingRate_MediumTopLeftAlert = current_chromosome[4:8]                 
-  closingRate_MediumTopLeftAlertValue = transform_fuzzy(closingRate_MediumTopLeftAlert, 1, closingRate_SlowTopAlertValue, closingRate_SlowTopAlertValue+16) 
+  closingRate_MediumTopLeftAlertValue = transform_fuzzy(closingRate_MediumTopLeftAlert, 1, (closingRate_SlowTopAlertValue+1), closingRate_SlowTopAlertValue+16) 
   closingRate_MediumTopRightAlert = current_chromosome[8:12]                 
-  closingRate_MediumTopRightAlertValue = transform_fuzzy(closingRate_MediumTopRightAlert, 1, closingRate_MediumTopLeftAlertValue, closingRate_MediumTopLeftAlertValue+16) 
+  closingRate_MediumTopRightAlertValue = transform_fuzzy(closingRate_MediumTopRightAlert, 1, (closingRate_MediumTopLeftAlertValue+1), closingRate_MediumTopLeftAlertValue+16) 
   closingRate_FastTopAlert = current_chromosome[12:16]                 
-  closingRate_FastTopAlertValue = transform_fuzzy(closingRate_FastTopAlert, 1, closingRate_MediumTopRightAlertValue, closingRate_MediumTopRightAlertValue+16) 
+  closingRate_FastTopAlertValue = transform_fuzzy(closingRate_FastTopAlert, 1, (closingRate_MediumTopRightAlertValue+1), closingRate_MediumTopRightAlertValue+16) 
   
   print("closingRate_FastTopAlertValue", closingRate_FastTopAlertValue)
   closingRate_SlowBottomAlert = current_chromosome[16:20]  
