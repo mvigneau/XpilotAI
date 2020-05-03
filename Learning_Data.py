b0d6edc3cc6be012b349161c0722c75dc689d26e
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-03 15:23:09 -0400

    Collected Data output to excel data file.

--- Learning_Data.py
+++ Learning_Data.py
@@ -6,37 +6,32 @@ from xlutils.copy import copy
 
 def Save_Data(filename, sheet_number, titles, data, first_time):
 
-       print(filename)
        ## Open existing workbook ##
        book = open_workbook(filename)
-       #print("found file")
+
        ## Get the first sheet inside the workbook ##
        r_sheet = book.sheet_by_index(sheet_number)
 
 
        ## Copy the workbook read over into a workbook we can write into ##
        wb = copy(book)
-       #print("got new workbook")
+       
        ## Get the sheet to write to within the writable copy ##
        w_sheet = wb.get_sheet(sheet_number)
 
        ## find the first row that has no data (empty) ##
        number_rows = r_sheet.nrows
-       print("before", number_rows)
+
        ## write titles of each column at top ##
        if(first_time == True):
               for number in range(len(titles)):
                      w_sheet.write(number_rows, number, titles[number])
-                     print(titles[number])
               number_rows += 1
               first_time = False
-
-       print("after", number_rows)
 
        ## Write to writable sheet ##
        for col in range(len(data)):
               w_sheet.write(number_rows, col, data[col])
-              print(data[col])
 
        ## save the writable workbook 
        wb.save(filename)
