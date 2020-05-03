b2160af8c5c65f2bd5fa5645aa950ff85e47e8ef
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-03 14:41:57 -0400

    Collected Data output to excel data file.

--- Learning_Data.py
+++ Learning_Data.py
@@ -9,20 +9,20 @@ def Save_Data(filename, sheet_number, titles, data):
        print(filename)
        ## Open existing workbook ##
        book = open_workbook(filename)
-       print("found file")
+       #print("found file")
        ## Get the first sheet inside the workbook ##
        r_sheet = book.sheet_by_index(sheet_number)
 
 
        ## Copy the workbook read over into a workbook we can write into ##
        wb = copy(book)
-       print("got new workbook")
+       #print("got new workbook")
        ## Get the sheet to write to within the writable copy ##
-       w_sheet = wb.get_sheet(sheet_number)
+       #w_sheet = wb.get_sheet(sheet_number)
 
        ## find the first row that has no data (empty) ##
        number_rows = r_sheet.nrows
-       print(number_rows)
+       #print(number_rows)
 
        ## write titles of each column at top ##
        if number_rows == 0:
@@ -31,7 +31,7 @@ def Save_Data(filename, sheet_number, titles, data):
                      print(titles[number])
 
        ## Write to writable sheet ##
-       for col in range(data):
+       for col in range(len(data)):
               w_sheet.write(number_rows, col, data[col])
               print(data[col])
 
