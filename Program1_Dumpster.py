8d73405773f6aca91902454fd47f1549038ea727
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-06 12:31:44 -0400

    Adding TigerKing and multiple data files.

---
+++ Program1_Dumpster.py
@@ -0,0 +1,78 @@
+### Program 1 -- Mathieu Vigneault,  
+import libpyAI as ai
+def AI_loop():
+  #Release keys
+  ai.thrust(0)
+  ai.turnLeft(0)
+  ai.turnRight(0)
+  #Set variables
+  heading = int(ai.selfHeadingDeg())
+  tracking = int(ai.selfTrackingDeg())
+  frontWall = ai.wallFeeler(500,heading)
+  left45Wall = ai.wallFeeler(500,heading+45)
+  right45Wall = ai.wallFeeler(500,heading-45)
+  left90Wall = ai.wallFeeler(500,heading+90)
+  right90Wall = ai.wallFeeler(500,heading-90)
+  left135Wall = ai.wallFeeler(500,heading+135)
+  right135Wall = ai.wallFeeler(500,heading-135)
+  backWall = ai.wallFeeler(500,heading-180) 
+  trackWall = ai.wallFeeler(500,tracking)
+  
+  
+  #######   Shooting Ennemies  ########
+  ##Find the closest ennemy##
+  ClosestID = ai.closestShipId()
+  #print(ClosestID)
+  ##Get the closest ennemy direction and speed##
+  ClosestSpeed = ai.enemySpeedId(ClosestID)
+  #print(ClosestSpeed)
+  ClosestDir = ai.enemyTrackingDegId(ClosestID)
+  #print(ClosestDir)
+  ## Get the lockheadingdeg ##
+  enemy = ai.lockNext()
+  print(enemy)
+  head = ai.lockHeadingDeg()
+  print(head)
+  enemyDist = ai.selfLockDist()
+  print(enemyDist)
+  
+  ### Turning Rules ###
+  if frontWall <= 200 and (left45Wall < right45Wall): 
+    print("turning right")
+    ai.turnRight(1)
+  elif frontWall <= 200 and (left45Wall > right45Wall):
+    ai.turnLeft(1)
+  elif left90Wall <= 200:
+    print("turning right")
+    ai.turnRight(1) 
+  elif right90Wall <= 200:
+    print("turning left")
+    ai.turnLeft(1)
+  ### Thrust commands ####
+  elif ai.selfSpeed() <= 10 and (frontWall >= 200) and (left45Wall >= 200) and (right45Wall >= 200) and (right90Wall >= 200) and (left90Wall >= 200) and (left135Wall >= 50) and (right135Wall >= 50) and (backWall >= 50):
+    print("go forward")
+    ai.thrust(1)
+  elif trackWall < 75 and ai.selfSpeed() >= 10:
+    ai.thrust(1)
+  elif trackWall < 50 and ai.selfSpeed() <= 10:
+    ai.thrust(1)
+  elif backWall <= 75:
+    ai.thrust(1)  
+  elif left135Wall <= 75:
+    ai.thrust(1)
+  elif right135Wall <= 75:
+    ai.thrust(1)
+  ##### Shooting Ennemy Commands #####
+  elif enemyDist <= 500 and heading > (head):
+    ai.turnRight(1)
+    ai.fireShot()
+  elif enemyDist <= 500 and heading < (head):
+    ai.turnLeft(1)
+    ai.fireShot()
+  else:
+    print("chilling")
+    ai.thrust(0)
+  
+  
+
+ai.start(AI_loop,["-name","Dubster","-join","localhost"])
