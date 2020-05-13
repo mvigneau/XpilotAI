a0cb4a2d8c569a7ab9c35d176922111e541c38e6
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-13 13:03:38 -0400

    Dumpsterr fixing

--- Dumpster_Learning.py
+++ Dumpster_Learning.py
@@ -221,16 +221,16 @@ def AI_loop():
       count_frame = 0
       generation += 1
       fitness_list.clear()
-      print(loop)
       if generation == generation_size:
         print("Done")
         quitAI()
         ### DONE -- QUIT ###
 
-      else:   
-        loop += 1 
-        count_frame = 0
-        boolean = True
+    else:  
+      print(loop) 
+      loop += 1 
+      count_frame = 0
+      boolean = True
 
   ### Rules ###
   else:
@@ -281,6 +281,7 @@ def AI_loop():
 
       count_frame += 3
       boolean = False
+      print("alive")
       
 
 ai.headlessMode()
