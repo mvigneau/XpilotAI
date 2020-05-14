5ccf267d793e5712a7d3e5d5801e5023d95a2ac3
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-13 21:59:40 -0400

    Dumpsterr fixing

--- Dumpster_Learning.py
+++ Dumpster_Learning.py
@@ -208,16 +208,16 @@ def AI_loop():
       titles = ["Generation", "Average Fitness", "Best Fitness","Population Size", "Chromosome Size", "Crossover Probability", "Mutation Probability", "Best Chromosome", "Entire Population Chromosome", "Entire Population Fitness"]
       data = [generation, statistics.mean(fitness_list), max(fitness_list), population_size, chromosome_size, crossover_prob, mutation_prob, string_maxChromosome, string_population, string_fitness]
       first_time = Save_Data("Dumpster_Training_Data.xls", 0, titles, data, first_time)
-
+      print("done savings")
       ## Select Next Generation -- Apply Crossover & Mutation ##
       new_population = select(population, fitness_list)
-      #print("new", new_population)
+      print("new", new_population)
       new_population = crossover(new_population, chromosome_size, population_size, crossover_prob)
-      #print("crossover", new_population)
+      print("crossover", new_population)
       new_population = mutate(new_population, chromosome_size, mutation_prob)
-      #print("mutate", new_population)
+      print("mutate", new_population)
       population = new_population
-      #print("population", population)
+      print("population", population)
 
       loop = 0
       count_frame = 0
