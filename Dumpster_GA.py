a1a568ba4f6b77ec18b4921d90f1ec822d188a47
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-14 00:09:38 -0400

    Dumpsterr fixing

--- Dumpster_GA.py
+++ Dumpster_GA.py
@@ -29,7 +29,7 @@ def fitness(chromosome, frames, score_previous, score_current):
 	if(abs(score_previous - score_current) == 3.4):
 		value -= 50
 	elif((score_current - score_previous) > 0):
-		value += (1000 / (frames/100))
+		value += 300
 	else:
 		value -= 50
 
