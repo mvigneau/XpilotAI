2a26404e1ecdf020204628d807e1733fcdb5d5b0
mvigneau <59557125+mvigneau@users.noreply.github.com>
2020-05-14 11:51:58 -0400

    Dumpster Agent New

--- Dumpster_Agent1.py
+++ Dumpster_Agent1.py
@@ -27,10 +27,7 @@ def AI_loop():
   ai.turnRight(0)
 
   ### Order of chromosome do not matter when created ###
-  daddy = [[0,0,0,0,1,0,0,1,1,1,0,0,0,1,1,0,0,0,0,1,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,1,1,1,1,1,0,1,0,0,1,0,1,0,1,1,1,1]]
-  population = []
-  population.append(daddy[0])
-  current_chromosome = population[loop]
+  current_chromosome = [0,0,0,0,1,0,0,1,1,1,0,0,0,1,1,0,0,0,0,1,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,1,1,1,1,1,0,1,0,0,1,0,1,0,1,1,1,1]
   print(current_chromosome)
 
   closingRate_SlowBottomAlert = current_chromosome[16:20]  
